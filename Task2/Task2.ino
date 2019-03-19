078d098b2708410f4b74ecf302d51a52963c5187
Ancelin George P <30872495+Ancelin1997@users.noreply.github.com>
2019-03-19 11:21:06 +0530

    Add files via upload

---
+++ Task2/Task2.ino
@@ -0,0 +1,31 @@
+const int hallPin = 12;     
+const int ledPin =  13;
+  int enable2 = 9;
+int enable1=8;
+int in3 = 10;
+int in4 = 11;
+int in1 = 4;
+int in2 = 5;
+int hallState = 0;      
+
+void setup() {
+  pinMode(hallPin, INPUT);  
+  pinMode(enable1, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);   
+}
+void loop(){
+  hallState = digitalRead(hallPin);
+
+  if (hallState == LOW) {     
+    analogWrite(enable1, 255); 
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2,LOW);    
+    digitalWrite(ledPin, HIGH);  
+  } 
+  else {
+    analogWrite(enable1,0 );
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2,LOW);
+  }
+}
